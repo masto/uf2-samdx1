dc4245602f9a5ed7ae9f9465fbf1889f4fb4ea54
Christopher Masto <chris@masto.com>
2023-05-09 19:35:20 -0400

    Add a config for NABU USB converter
    Copied from Trinket M0 with the neopixel turned off.

---
+++ boards/nabu_usb/board_config.h
@@ -0,0 +1,22 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Christopher Masto"
+#define PRODUCT_NAME "NABU USB Converter"
+#define VOLUME_LABEL "NABU_USB"
+#define INDEX_URL "http://adafru.it/3500"
+#define BOARD_ID "SAMD21E18A-NABUUSB-v0"
+
+#define USB_VID 0x239A
+#define USB_PID 0x001E
+
+#define LED_PIN PIN_PA10
+//#define LED_TX_PIN PIN_PA27
+//#define LED_RX_PIN PIN_PB03
+
+//#define BOARD_RGBLED_CLOCK_PIN            PIN_PA01
+//#define BOARD_RGBLED_DATA_PIN             PIN_PA00
+
+#endif
